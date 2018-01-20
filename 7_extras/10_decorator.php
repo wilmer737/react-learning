14b96ae30dbcce648a06c3ce2ab1723b0a074842
Justin Black <justin@targetcw.com>
2018-01-19 16:06:40 -0800

    reset master

--- 7_extras/10_decorator.php
+++
@@ -1,9 +0,0 @@
-<?php
-$auto = new Automobile();
-
-$model = new BaseAutoModel();
-$model = new SportAutoModel($model);
-$model = new TouringAutoModel($model);
-
-$auto->setModel($model);
-$auto->printDescription();
--- 7_extras/2_gateway.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class CustomerGateway
-{
-    public function getCustomer(int $id) {
-        // Database functionality to retrieve the customer.
-        return new Customer();
-    }
-}
